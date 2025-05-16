a87fb3cd5282d3aae194c867fb708f6182bab109
unpetrifiedstone2005 <arya.duttagupta1@gmail.com>
2025-05-16 20:48:24 +0530

    authmiddleware added

---
+++ backend/middlewares/authmiddleware.js
@@ -0,0 +1,23 @@
+const JWT_SECRET = require("../config");
+const jwt = require("jsonwebtoken");
+
+export const authmiddleware = (req,res,next)=>{
+  const authHeader = req.body.authorization;
+
+  if(!authHeader || !authHeader.startsWith('Bearer: '))
+    return res.status(411).json({msg:"incorrect auth"})
+
+  const token = authHeader.split(' ')[1];
+
+  try{
+    const decoded = jwt.verify(token, JWT_SECRET);
+    req.userId = decoded.userId;
+    next();
+
+  }catch(err){
+    return res.status(411).json({
+      error: err
+    })
+  }
+}
+
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,97 @@
 const express = require("express");
-
+const zod = require("zod");
+const jwt = require("jsonwebtoken");
+const {User} = require("../db");
+const JWT_SECRET =  require("../config");
+const authmiddleware = require("./middlewares/authmiddleware");
 const router = express.Router();
 
 const app  = express();
 
+const signupSchema = zod.object({
+  username: zod.string(),
+  password: zod.string(),
+  firstName: zod.string(),
+  lastName: zod.string()
+})
 
+router.post("/signup", async (req,res) => {
+ const body = req.body;
+ const {success} = signupSchema.safeParse(body);
+
+  if(!success){
+    return res.status(411).json({
+      message: "schema incorrect"
+    })
+  }
+
+  const user = await User.findOne({
+    username: body.username
+  })
+
+  if(user){
+    return res.status(411).json({
+      message: "Username already exists"
+    })
+  }
+
+  const newUser = await User.create({
+    username: body.username,
+    password: body.password,
+    firstName: body.firstName,
+    lastName: body.firstName
+  })
+
+  const userId = newUser._id;
+
+  const token = jwt.sign({
+    userId
+  }, JWT_SECRET);
+
+
+  return res.status(200).json({
+    message: "User created successfully",
+    token: token
+  })
+ 
+})
+
+
+const signinSchema = zod.object({
+    username: zod.string(),
+    password: zod.string()
+})
+
+app.post("/signin", async (req,res) => {
+  const body = req.body;
+  
+  const {success} = signinSchema.safeParse(body);
+
+  if(!success){
+    res.status(411).json({
+      message: "Incorrect schema"
+    })
+  }
+
+  const user = await User.findOne({
+    username: body.username,
+    password: body.password,
+  })
+  
+  if(!user){
+    res.status(411).json({msg: "User doesn't exist"})
+  }
+  
+  const token = jwt.sign({
+    userId: user._id
+  }, JWT_SECRET)
+
+  return res.status(200).json({
+    message: "User found successfully",
+    token: token
+  })
+  
+  
+})
 
 module.exports =  router;
