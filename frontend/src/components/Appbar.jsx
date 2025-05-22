d67674b178ca1a9534e6a7b5e7718e91c070bda4
unpetrifiedstone2005 <arya.duttagupta1@gmail.com>
2025-05-22 19:07:22 +0530

    added button for user update endpoint

--- frontend/src/components/Appbar.jsx
+++ frontend/src/components/Appbar.jsx
@@ -9,11 +9,9 @@ export function Appbar(){
       <div className="flex flex-col font-medium justify-center h-full mr-4">
         Welcome
       </div>
-    <div className="rounded-full h-12 w-12 bg-slate-600 mt-1 mr-2">
-      <div className="flex flex-col text-slate-200 font-medium items-center justify-center h-full text-xl">
+    <button className="rounded-full h-12 w-12 bg-slate-600 hover:bg-slate-800 mt-1 mr-2 text-slate-200 font-medium ">
         U
-      </div>
-    </div>
+    </button>
     <div className="flex items-center px-3">
       <Link to = {"/"}><button className="text-white bg-red-700 font-medium rounded px-2 py-1 hover:bg-red-800">Log out</button></Link>
       </div>
