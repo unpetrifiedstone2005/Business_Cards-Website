ec213a17493380079d9213f815946807e9df19c2
unpetrifiedstone2005 <arya.duttagupta1@gmail.com>
2025-05-25 18:31:58 +0530

    backend changed for prod and dev

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json())
 const mainRouter = require("./routes/index")
@@ -9,4 +10,6 @@ const mainRouter = require("./routes/index")
 
 app.use("/api/v1", mainRouter);
 
-app.listen(3000);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
